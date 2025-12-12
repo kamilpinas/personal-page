7e798f277bf1f7d6749d7cd703239428d83d650e
Kamil Pinas <k.pinas@softint.pl>
2025-12-12 20:14:39 +0100

    sitemap

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,11 +1,16 @@
-const fs = require("fs")
-const path = require("path")
+import fs from "fs"
+import path, { dirname } from "path"
+import { fileURLToPath } from "url"
 
+// Get __dirname in ES module
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+// Config
 const BASE_URL = "https://kamilpinas.dev"
 const LANGUAGES = ["en", "pl"]
-
-const routes = [
-  "", // Home page
+const ROUTES = [
+  "",
   "/about",
   "/projects",
   "/skills",
@@ -13,30 +18,40 @@ const routes = [
   "/contact",
   "/resume",
 ]
+const OUTPUT_PATH = path.resolve(
+  __dirname,
+  "../../personal-page/public/sitemap.xml"
+)
 
-let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+/**
+ * Generates a sitemap.xml file for the site
+ * @param {string} baseUrl - Base URL of the site
+ * @param {string[]} languages - Array of language codes
+ * @param {string[]} routes - Array of site routes
+ * @param {string} outputPath - Full path to save sitemap.xml
+ */
+function generateSitemap(baseUrl, languages, routes, outputPath) {
+  let sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`
 
-LANGUAGES.forEach((lang) => {
-  routes.forEach((route) => {
-    const url = `${BASE_URL}/${lang}${route}`
-    sitemap += `
+  languages.forEach((lang) => {
+    routes.forEach((route) => {
+      const url = `${baseUrl}/${lang}${route}`
+      sitemap += `
   <url>
     <loc>${url}</loc>
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>`
+    })
   })
-})
 
-sitemap += `
+  sitemap += `
 </urlset>`
 
-const outputPath = path.resolve(
-  __dirname,
-  "../../personal-page/public/sitemap.xml"
-)
+  fs.writeFileSync(outputPath, sitemap, "utf8")
+  console.log(`sitemap.xml generated successfully at: ${outputPath}`)
+}
 
-fs.writeFileSync(outputPath, sitemap, "utf8")
-
-console.log("sitemap.xml generated successfully!")
+// Run the function
+generateSitemap(BASE_URL, LANGUAGES, ROUTES, OUTPUT_PATH)
